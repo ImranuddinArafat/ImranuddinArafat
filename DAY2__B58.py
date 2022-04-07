85207a91aae9f7783bbf0ff114e684fa0c2c5ef8
ImranuddinArafat <103121316+ImranuddinArafat@users.noreply.github.com>
2022-04-07 17:46:28 +0530

    Add files via upload

---
+++ DAY2__B58.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x = 3
+y = 6
+
+
+# In[2]:
+
+
+x+y
+
+
+# In[ ]:
+
+
+Rules for declaring the variables in python
+
+
+# In[ ]:
+
+
+1.Rule no:A variable  should not contain any namespaces
+
+
+# In[3]:
+
+
+first name ='imran'
+print(first name)
+
+
+# In[5]:
+
+
+firstname = 'imran'
+print(firstname)
+
+
+# In[6]:
+
+
+first_name ='imran'
+print(first_name)
+
+
+# In[ ]:
+
+
+Rule 2: A variable name should not start with a number 
+
+
+# In[7]:
+
+
+1name = 'imran'
+print(1name)
+
+
+# In[8]:
+
+
+name1 = 'imran'
+print(name1)
+
+
+# In[ ]:
+
+
+Rule 3: A variable  name should not contain any special characters
+
+
+# In[ ]:
+
+
+$@&^
+
+
+# In[9]:
+
+
+name@ = 'jack'
+print(name@)
+
+
+# In[ ]:
+
+
+introduction to data types
+1.String------str
+2.Numbers---integers(int) and float(float)
+3.list----list
+4.tuple---tuple
+5.Dictionaries--dict
+
+
+# In[ ]:
+
+
+classification of datatypes
+1.mutuable---which can be edit or alter---flexible
+2.immutuable--which cannot edit or alter---fixed
+
+
+# In[ ]:
+
+
+Introduction to string data type
+Def: A string is series of char declared in quotes
+    it is classified as an immutable 
+how to declare the stringh data type ---3 possible options for declaring the string
+1.single quote
+2.Double quote
+3.triple quote
+
+
+# In[10]:
+
+
+name = 'imran'
+print(name)
+
+
+# In[14]:
+
+
+name2 = "azhar"
+print(name2)
+
+
+# In[15]:
+
+
+name3 = """imtiyaz"""
+print(name3)
+
+
+# In[ ]:
+
+
+# is used for commenting the code
+
+
+# In[ ]:
+
+
+introduction to string methods
+
+
+# In[16]:
+
+
+type(name)
+
+
+# In[17]:
+
+
+type(name2)
+
+
+# In[18]:
+
+
+type(name3)
+
+
+# In[ ]:
+
+
+
+
