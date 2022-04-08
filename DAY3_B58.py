fa2ec75ddcc170b5a4342b97a25f08391a146072
ImranuddinArafat <103121316+ImranuddinArafat@users.noreply.github.com>
2022-04-08 21:17:39 +0530

    Add files via upload

---
+++ DAY3_B58.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+full_name ='imran uddin'
+print(full_name)
+
+
+# In[2]:
+
+
+full_name ='imran uddin'
+print(full_name.title())
+
+
+# In[3]:
+
+
+full_name ='imran uddin'
+print(full_name.upper())
+
+
+# In[4]:
+
+
+full_name ='imran uddin'
+print(full_name.lower())
+
+
+# In[ ]:
+
+
+introduction of f strings:
+
+
+# In[ ]:
+
+
+f' custom words {placeholder1} {placeholder2} .....{placeholder n}
+
+
+# In[12]:
+
+
+first_name = 'imran'
+last_name = 'uddin'
+
+
+# In[13]:
+
+
+full_name = f"{first_name} {last_name}"
+print(full_name)
+
+
+# In[14]:
+
+
+message = f"keep up the good work, {full_name}"
+print(message)
+
+
+# In[15]:
+
+
+message = f"keep up the good work, {full_name.title()}"
+print(message)
+
+
+# In[ ]:
+
+
+Adding white spaces to strings
+
+
+# In[16]:
+
+
+print('favourite_language:pythonc++javacswiftjavascript')
+
+
+# In[17]:
+
+
+print('favourite_language:\npython\nc++\njava\ncswift\njavascript')
+
+
+# In[ ]:
+
+
+\n ==== it is calles as new line  delimeter
+
+
+# In[18]:
+
+
+print('favourite_language:\n\tpython\n\tc++\n\tjava\n\tcswift\n\tjavascript')
+
+
+# In[ ]:
+
+
+\t ==== it is calles as tab delimeter
+
+
+# In[ ]:
+
+
+Removing whitespaces from strings
+
+
+# In[19]:
+
+
+name = 'python'
+print(name)
+
+
+# In[22]:
+
+
+name2 = '  java'
+print(name2)
+
+
+# In[24]:
+
+
+name3 = 'c++  '
+print(name3)
+
+
+# In[26]:
+
+
+name2.lstrip()
+
+
+# In[28]:
+
+
+name3.rstrip()
+
+
+# In[ ]:
+
+
+
+
