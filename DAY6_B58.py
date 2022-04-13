10f7118c159fb9fffef139f131663825f40d84a3
ImranuddinArafat <103121316+ImranuddinArafat@users.noreply.github.com>
2022-04-13 17:27:53 +0530

    Add files via upload

---
+++ DAY6_B58.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+INTRODUCTION TO LOOPING STATEMENTS:
+
+
+# In[ ]:
+
+
+Understanding the concept of for loop
+
+
+# In[1]:
+
+
+students = ['simrah','ehan','arafat','arsalaan','ayesha','mysha']
+
+
+# In[2]:
+
+
+print(students)
+
+
+# In[ ]:
+
+
+ist approach
+
+
+# In[4]:
+
+
+message = f'keep up the good work,{students[0]}'
+
+print(message)
+
+
+# In[5]:
+
+
+message = f'keep up the good work,{students[2]}'
+
+print(message)
+
+
+# In[6]:
+
+
+message = f'keep up the good work,{students[4].title()}'
+
+print(message)
+
+
+# In[ ]:
+
+
+# genral syntax of a for loop:
+
+
+# In[ ]:
+
+
+for tempvar in mainvar:
+    print(tempvar)
+
+
+# In[9]:
+
+
+for x in students:
+    print(f'keep up the good work,{x.title()}')
+
+
+# In[10]:
+
+
+for spiderman in students:
+    print(f'keep up the good work,{spiderman.title()}')
+
+
+# In[11]:
+
+
+for x in students:
+    print(f'keep up the good work,{x.title()}')
+    print(f'i will be looking forward to receive your github urls of today,{x.title()}') 
+
+
+# In[12]:
+
+
+for x in students:
+    print(f'keep up the good work,{x.title()}')
+    print(f'i will be looking forward to receive your github urls of today,{x.title()}\n') 
+
+
+# In[13]:
+
+
+for x in students:
+    print(f'keep up the good work,{x.title()}')
+    print(f'i will be looking forward to receive your github urls of today,{x.title()}\n') 
+    
+print('Thank you all of you for joining the python training session')
+
+
+# In[ ]:
+
+
+for tempvar in mainvar
+    print(tempvar)
+    
+# indentation
+
+
+# In[14]:
+
+
+for tempvar in mainvar:
+print(tempvar)
+
+
+# In[ ]:
+
+
+
+
