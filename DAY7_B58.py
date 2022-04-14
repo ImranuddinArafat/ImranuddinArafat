7824182b653337be38018143a6119afa0742c100
ImranuddinArafat <103121316+ImranuddinArafat@users.noreply.github.com>
2022-04-14 21:59:35 +0530

    Add files via upload

---
+++ DAY7_B58.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+INTRODUCTION TO Tuple data type :
+
+
+# In[ ]:
+
+
+defination: an immutable list is called as tuple data type
+classification: it is classified as  an immutable datatype
+how do u define the tuple data type------------>()
+
+
+# In[1]:
+
+
+students =('imran','simrah','arafat','ehan','nikhath','amoon')
+
+
+# In[2]:
+
+
+print(students)
+
+
+# In[3]:
+
+
+type(students)
+
+
+# In[ ]:
+
+
+# req : i want to change imran----> imtiaz
+
+
+# In[4]:
+
+
+students[0] = 'imtiaz'
+
+
+# In[ ]:
+
+
+
+
+
+# In[8]:
+
+
+dimensions = (250,50)
+
+print(dimensions)
+
+
+# In[ ]:
+
+
+# req : i want to change the dimension from 200,250..?
+
+
+# In[9]:
+
+
+dimensions = (200,50)
+
+print(dimensions)
+
+
+# In[ ]:
+
+
+# land-->house---->blueprint---->hall---->bedrooms---->kitchen--->fixed data
+
+6 months
+
+
+# In[10]:
+
+
+print(students)
+
+
+# In[ ]:
+
+
+# req : i want to get the simrah name in thre output
+
+
+# In[11]:
+
+
+print(students[1])
+
+
+# In[ ]:
+
+
+# req : i want to implement a for loop on to the tuple..?
+
+
+# In[18]:
+
+
+for x in students:
+ print(x.title())
+
+
+# In[ ]:
+
+
+
+
